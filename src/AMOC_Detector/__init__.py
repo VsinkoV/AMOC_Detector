0908989ab500e8f58f83b11b6a395d906beb42df
Harry Ellingham <ellinghh@lancaster.ac.uk>
2026-08-05 14:03:15 +0100

    added kfdr

--- src/AMOC_Detector/__init__.py
+++ src/AMOC_Detector/__init__.py
@@ -3,11 +3,14 @@ from .costs.caussian_cim import gaussian_mean_change
 from .costs.RBF import rbf_kernel, _median_heuristic as median_heuristic
 from .costs.laplacian_kernel import Laplacian_kernel
 from .costs.caussian_cim import gaussian_variance_change
+from .costs.kfdr import kfdr
+
 __all__ = [
     "Detector",
     "gaussian_mean_change",
     "rbf_kernel",
     "median_heuristic",
     "Laplacian_kernel",
-    "gaussian_variance_change"
+    "gaussian_variance_change",
+    "kfdr"
 ]
